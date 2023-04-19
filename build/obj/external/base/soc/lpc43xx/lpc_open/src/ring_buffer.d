eee6b5aea27c2aa1d1e0c7f09be0cbf1e0c2a15c
Martin1798 <martnluque1798@gmail.com>
2023-04-19 15:16:15 -0300

    Listo para usar

---
+++ build/obj/external/base/soc/lpc43xx/lpc_open/src/ring_buffer.d
@@ -0,0 +1,4 @@
+build/obj/external/base/soc/lpc43xx/lpc_open/src/ring_buffer.o: \
+ /home/estudiante/Desktop/LABMICRO/Laboratorio-micro/Plantilla/plantilla-embebidos/muju/external/base/soc/lpc43xx/lpc_open/src/ring_buffer.c \
+ /home/estudiante/Desktop/LABMICRO/Laboratorio-micro/Plantilla/plantilla-embebidos/muju/external/base/soc/lpc43xx/lpc_open/inc/ring_buffer.h \
+ /home/estudiante/Desktop/LABMICRO/Laboratorio-micro/Plantilla/plantilla-embebidos/muju/external/base/soc/lpc43xx/lpc_open/inc/lpc_types.h
---
+++ build/obj/external/base/soc/lpc43xx/startup/cr_startup_lpc43xx.d
@@ -0,0 +1,2 @@
+build/obj/external/base/soc/lpc43xx/startup/cr_startup_lpc43xx.o: \
+ /home/estudiante/Desktop/LABMICRO/Laboratorio-micro/Plantilla/plantilla-embebidos/muju/external/base/soc/lpc43xx/startup/cr_startup_lpc43xx.c
